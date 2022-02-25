3a6a3ac41ff06fda649bb83bdc907b0a56335378
HARDTACK-Dev <rjsdn827@gmail.com>
2022-02-25 18:07:02 +0900

    Initial commit
    Very basic

---
+++ tm-manager/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css'
+
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+Vue.use(Vuesax, {})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
